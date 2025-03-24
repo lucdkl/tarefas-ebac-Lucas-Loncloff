8dc2b730a827db855e9b4b31fe80318b7aa76935
L A <lucasdeliperi@gmail.com>
2025-03-24 14:33:51 -0300

    PIOR ERRO USAR JAVA FX PORQUE EU DECIDI FAZER ISSO

---
+++ mod17-JavaFX-Abstract And Generics/src/main/java/com/vkl/Executavel.java
@@ -0,0 +1,9 @@
+package com.vkl;
+
+import javafx.application.Application;
+
+public class Executavel {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
