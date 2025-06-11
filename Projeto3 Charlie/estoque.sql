a2416edfdbd80eba38386c4137f08fdc72e5d5ea
Lucvkl <lucasdeliperi@gmail.com>
2025-06-11 19:08:17 -0300

    Update estoque.sql

--- Projeto3 Charlie/estoque.sql
+++ Projeto3 Charlie/estoque.sql
@@ -3,8 +3,15 @@ create table tb_produto_estoque(
 	id_produto_fk bigint not null,
 	quantidade int not null,
 	constraint pk_id_prod_estoque primary key(id),
-	constraint fk_id_prod_estoque foreign key(id_produto_fk) references tb_produto(id)
+	constraint fk_id_prod_estoque foreign key(id_produto_fk) references tb_produto(id) on delete cascade
 );
+
+ALTER TABLE tb_produto_estoque
+DROP CONSTRAINT fk_id_prod_estoque,
+ADD CONSTRAINT fk_id_prod_estoque 
+    FOREIGN KEY (id_produto_fk) 
+    REFERENCES tb_produto(id) 
+    ON DELETE CASCADE;
 
 truncate tb_produto cascade
 
@@ -13,7 +20,8 @@ start 1
 increment 1
 owned by tb_produto_estoque.id;
 
-truncate tb_produto cascade
+truncate tb_cliente cascade;
+truncate tb_produto cascade;
 
 select * from tb_produto;
 
