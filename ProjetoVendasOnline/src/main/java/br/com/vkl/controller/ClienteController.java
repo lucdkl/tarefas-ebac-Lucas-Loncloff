6310de1cf9579df0cf7925a5c0e4931ceaa7f7fd
Lucvkl <lucasdeliperi@gmail.com>
2025-07-31 13:18:29 -0300

    finalized this

--- ProjetoVendasOnline/src/main/java/br/com/vkl/controller/ClienteController.java
+++ ProjetoVendasOnline/src/main/java/br/com/vkl/controller/ClienteController.java
@@ -44,6 +44,59 @@ private Cliente cliente;
 			FacesContext.getCurrentInstance().addMessage("growl", new FacesMessage("Erro ao tentar listar os clientes"));
 		}
 	}
+	
+	public void cancel() {
+		try {
+			this.isUpdate = false;
+			this.cliente = new Cliente();
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage("growl", new FacesMessage("Erro ao tentar cancelar ação"));
+		}
+		
+    } 
+	
+	public void edit(Cliente cliente) {
+		try {
+			this.isUpdate = true;
+			this.cliente = cliente;
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage("growl", new FacesMessage("Erro ao tentar excluir o cliente"));
+		}
+		
+    } 
+	
+	public void delete(Cliente cliente) {
+		try {
+			clienteService.excluir(cliente);
+			clientes.remove(cliente);
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage("growl", new FacesMessage("Erro ao tentar excluir o cliente"));
+		}
+		
+    } 
+	
+	public void add() {
+		try {
+			clienteService.cadastrar(cliente);
+			this.clientes = clienteService.buscarTodos();
+			this.cliente = new Cliente();
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage("growl", new FacesMessage("Erro ao tentar criar o cliente"));
+		}
+		
+        
+    }
+
+    public void update() {
+    	try {
+			clienteService.alterar(this.cliente);
+			cancel();
+			FacesContext.getCurrentInstance().addMessage("growl", new FacesMessage("Cliente Atualiado com sucesso"));
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage("growl", new FacesMessage("Erro ao tentar atualizar o cliente"));
+		}
+        
+    }
 
 	public Cliente getCliente() {
 		return cliente;
