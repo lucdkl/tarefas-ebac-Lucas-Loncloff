c0fe4fec291002ca1b5fc344ea8a1b14f27fb262
Lucvkl <lucasdeliperi@gmail.com>
2026-03-09 14:56:20 -0300

    mod 42 bac

---
+++ mod42Projeto4paraMicroServiços/ProdutoServiceAlunos/ProdutoServiceAlunos/src/main/java/com/br/vkl/vendas/online/ProdutoServiceAlunosApplication.java
@@ -0,0 +1,13 @@
+package com.br.vkl.vendas.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoServiceAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoServiceAlunosApplication.class, args);
+	}
+
+}
